b6d4a4517e68610a070de63691809804459735e3
Omar <omar@plusomar.com>
2025-08-20 00:02:51 +0300

    Updates

---
+++ assets/js/icons-C1-k-h3N.js
@@ -0,0 +1,21 @@
+import{r as o}from"./vendor-CG1BCU3E.js";/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var p={xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"};/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const w=e=>e.replace(/([a-z0-9])([A-Z])/g,"$1-$2").toLowerCase().trim(),s=(e,i)=>{const a=o.forwardRef(({color:c="currentColor",size:t=24,strokeWidth:n=2,absoluteStrokeWidth:l,className:y="",children:r,...u},d)=>o.createElement("svg",{ref:d,...p,width:t,height:t,stroke:c,strokeWidth:l?Number(n)*24/Number(t):n,className:["lucide",`lucide-${w(e)}`,y].join(" "),...u},[...i.map(([k,m])=>o.createElement(k,m)),...Array.isArray(r)?r:[r]]));return a.displayName=`${e}`,a};/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const b=s("Menu",[["line",{x1:"4",x2:"20",y1:"12",y2:"12",key:"1e0a9i"}],["line",{x1:"4",x2:"20",y1:"6",y2:"6",key:"1owob3"}],["line",{x1:"4",x2:"20",y1:"18",y2:"18",key:"yk5zj1"}]]);/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const h=s("X",[["path",{d:"M18 6 6 18",key:"1bl5f8"}],["path",{d:"m6 6 12 12",key:"d8bk6v"}]]);export{b as M,h as X};
